2c62915d02409dbaa9157da2330113770bd06cf9
Ismailchaoufi <147541365+Ismailchaoufi@users.noreply.github.com>
2024-05-18 12:57:31 +0200

    Delete JIBI directory

--- JIBI/src/main/java/com/ENSA/JIBI/repository/CompteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ENSA.JIBI.repository;
-
-import com.ENSA.JIBI.models.Compte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompteRepository extends JpaRepository<Compte, Long> {
-    Compte findByRib(String rib);
-}
